49b6d7ff1f4c25f13db723d2530d9628961822e2
lyudmilabystrova <ludmilabystrova@gmail.com>
2024-05-05 13:25:09 +0200

    made custom checkboxes

--- vite-project/src/components/TransferPanel/TransferPanel.tsx
+++ vite-project/src/components/TransferPanel/TransferPanel.tsx
@@ -6,30 +6,36 @@ return (
         <p className={styles["heading"]}>Количество пересадок</p>
         <form className={styles["transfer"]}>
             <div className={styles["field"]}>
-                   <label>
-                <input type='checkbox' name='transfers' value='Все'/>
+                   <label className={styles["check"]}>
+                <input className={styles["check__input"]} type='checkbox' name='transfers' value='Все'/>
+                 <span className={styles["check__box"]}></span>
                 Все
             </label>
             </div>
          <div className={styles["field"]}>
-              <label>
-                <input type='checkbox' name='transfers' value='Без пересадок'/>
+              <label className={styles["check"]}>
+                <input className={styles["check__input"]} type='checkbox' name='transfers' value='Без пересадок'/>
+                <span className={styles["check__box"]}></span>
                 Без пересадок
             </label>
          </div>
            <div className={styles["field"]}>
-                <label>
-                <input type='checkbox' name='transfers' value='1 пересадка'/>
+                <label className={styles["check"]}>
+                <input className={styles["check__input"]} type='checkbox' name='transfers' value='1 пересадка'/>
+                      <span className={styles["check__box"]}></span>
                 1 пересадка
             </label>
            </div>
-         <div className={styles["field"]}>  <label>
-                <input type='checkbox' name='transfers' value='2 пересадки'/>
+         <div className={styles["field"]}> 
+          <label className={styles["check"]}>
+                <input className={styles["check__input"]} type='checkbox' name='transfers' value='2 пересадки'/>
+                      <span className={styles["check__box"]}></span>
                 2 пересадки
             </label></div>
            <div className={styles["field"]}>
-               <label>
-                <input type='checkbox' name='transfers' value='3 пересадки'/>
+               <label className={styles["check"]}>
+                <input className={styles["check__input"]} type='checkbox' name='transfers' value='3 пересадки'/>
+                      <span className={styles["check__box"]}></span>
                 3 пересадки
             </label>
            </div>
